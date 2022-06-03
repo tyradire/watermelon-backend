550902b98d6c85734442b77d68249aa29ced82db
Daniel Ugarov <tyradire@yandex.ru>
2022-06-03 18:06:58 +0300

    git commit -m 'cors'

--- app.js
+++ app.js
@@ -14,13 +14,14 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
 app.use(appRouter);
 app.use(errors());
 app.use(sendError);
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).json({message: 'Working!'})
